ba1fc107e103a4c6eb62cf30e4b422a50ed8155a
Eva0104 <zhuxiaoxue0104@126.com>
2016-07-13 11:08:56 +0800

    使用SpringMVC的方式(ResponseEntity<InputStreamResource>)返回OutputStream

--- src/main/java/com/zhuxiaoxue/controller/FilePreviewController.java
+++ src/main/java/com/zhuxiaoxue/controller/FilePreviewController.java
@@ -1,14 +1,15 @@
 package com.zhuxiaoxue.controller;
 
 import com.zhuxiaoxue.exception.NotFoundException;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
 @Controller
@@ -19,23 +20,36 @@ public class FilePreviewController {
 
     /**
      * 从磁盘上读取文件，呈现给客户端
+     * @param //response
      * @param filename
-     * @param response
      * @throws IOException
      */
     @RequestMapping(value = "/preview/{filename}" ,method = RequestMethod.GET)
-    public void filePreview(@PathVariable String filename, HttpServletResponse response) throws IOException {
+//    public void filePreview(@PathVariable String filename, HttpServletResponse response) throws IOException {
+//        File file = new File(imageSavePath,filename);
+//        if(!file.exists()){
+//            throw new NotFoundException();
+//        }
+//
+//        FileInputStream inputStream = new FileInputStream(file);
+//        OutputStream outputStream = response.getOutputStream();
+//        IOUtils.copy(inputStream,outputStream);
+//
+//        outputStream.flush();
+//        outputStream.close();
+//        inputStream.close();
+//    }
+
+    public ResponseEntity<InputStreamResource> filePreview(@PathVariable String filename) throws IOException {
         File file = new File(imageSavePath,filename);
         if(!file.exists()){
             throw new NotFoundException();
         }
-
         FileInputStream inputStream = new FileInputStream(file);
-        OutputStream outputStream = response.getOutputStream();
-        IOUtils.copy(inputStream,outputStream);
 
-        outputStream.flush();
-        outputStream.close();
-        inputStream.close();
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(new InputStreamResource(inputStream));
     }
 }
---
+++ src/main/java/com/zhuxiaoxue/mapper/DocumentMapper.java
@@ -0,0 +1,9 @@
+package com.zhuxiaoxue.mapper;
+
+/**
+ * Created by Eric on 2016/7/13.
+ */
+public interface DocumentMapper {
+
+
+}
